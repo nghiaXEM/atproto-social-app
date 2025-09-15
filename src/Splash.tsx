f6ff894d77e6186a96aa3181ba237cfbdc18a973
tomarch <tommybuijr@gmail.com>
2025-09-15 11:45:38 +0700

    update logo

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -15,8 +15,7 @@ import Animated, {
   withTiming,
 } from 'react-native-reanimated'
 import {useSafeAreaInsets} from 'react-native-safe-area-context'
-import Svg, {Path, SvgProps} from 'react-native-svg'
-import {Image} from 'expo-image'
+import {Image, type ImageProps} from 'expo-image'
 import * as SplashScreen from 'expo-splash-screen'
 
 import {Logotype} from '#/view/icons/Logotype'
@@ -29,23 +28,41 @@ const darkSplashImageUri = RNImage.resolveAssetSource(
   darkSplashImagePointer,
 ).uri
 
-export const Logo = React.forwardRef(function LogoImpl(props: SvgProps, ref) {
+// export const Logo = React.forwardRef(function LogoImpl(props: SvgProps, ref) {
+//   const width = 1000
+//   const height = width * (67 / 64)
+//   return (
+//     <Svg
+//       fill="none"
+//       // @ts-ignore it's fiiiiine
+//       ref={ref}
+//       viewBox="0 0 64 66"
+//       style={[{width, height}, props.style]}>
+//       <Path
+//         fill={props.fill || '#fff'}
+//         d="M13.873 3.77C21.21 9.243 29.103 20.342 32 26.3v15.732c0-.335-.13.043-.41.858-1.512 4.414-7.418 21.642-20.923 7.87-7.111-7.252-3.819-14.503 9.125-16.692-7.405 1.252-15.73-.817-18.014-8.93C1.12 22.804 0 8.431 0 6.488 0-3.237 8.579-.18 13.873 3.77ZM50.127 3.77C42.79 9.243 34.897 20.342 32 26.3v15.732c0-.335.13.043.41.858 1.512 4.414 7.418 21.642 20.923 7.87 7.111-7.252 3.819-14.503-9.125-16.692 7.405 1.252 15.73-.817 18.014-8.93C62.88 22.804 64 8.431 64 6.488 64-3.237 55.422-.18 50.127 3.77Z"
+//       />
+//     </Svg>
+//   )
+// })
+export const Logo = React.forwardRef(function LogoImpl(
+  props: ImageProps,
+  ref: React.Ref<Image>
+) {
   const width = 1000
   const height = width * (67 / 64)
+
   return (
-    <Svg
-      fill="none"
-      // @ts-ignore it's fiiiiine
+    // eslint-disable-next-line react-native-a11y/has-valid-accessibility-ignores-invert-colors
+    <Image
+      // @ts-ignore
       ref={ref}
-      viewBox="0 0 64 66"
-      style={[{width, height}, props.style]}>
-      <Path
-        fill={props.fill || '#fff'}
-        d="M13.873 3.77C21.21 9.243 29.103 20.342 32 26.3v15.732c0-.335-.13.043-.41.858-1.512 4.414-7.418 21.642-20.923 7.87-7.111-7.252-3.819-14.503 9.125-16.692-7.405 1.252-15.73-.817-18.014-8.93C1.12 22.804 0 8.431 0 6.488 0-3.237 8.579-.18 13.873 3.77ZM50.127 3.77C42.79 9.243 34.897 20.342 32 26.3v15.732c0-.335.13.043.41.858 1.512 4.414 7.418 21.642 20.923 7.87 7.111-7.252 3.819-14.503-9.125-16.692 7.405 1.252 15.73-.817 18.014-8.93C62.88 22.804 64 8.431 64 6.488 64-3.237 55.422-.18 50.127 3.77Z"
-      />
-    </Svg>
+      source={require('../assets/logo.svg')}
+      style={[{ width, height, resizeMode: 'contain' }, props.style]}
+    />
   )
 })
+
 
 type Props = {
   isReady: boolean
@@ -234,7 +251,7 @@ export function Splash(props: React.PropsWithChildren<Props>) {
                 },
               ]}>
               <Animated.View style={[logoAnimations]}>
-                <Logo fill={logoBg} />
+                <Logo />
               </Animated.View>
             </Animated.View>
           )}
